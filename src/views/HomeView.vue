85eebd03b308a0103d2d572e43c382622aa10db2
fotis <fotis@arx.net>
2023-11-08 15:19:24 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue"
+
+const showText = ref(true)
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <Transition name="fade">
+        <h1 id="hey" v-if="showText">Hey</h1>
+        <h1 id="bye" v-else>GoodBye</h1>
+      </Transition>
+    </div>
+   <button @click="showText = !showText">Toggle</button>
+  </main>
+</template>
+
+
+<style scoped>
+
+.fade-enter-from {
+ opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 5s ease;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: all 3.5s ease-out;
+}
+
+.container {
+  position: relative;
+
+}
+
+h1{
+  position: absolute;
+}
+button{
+  margin-top: 50px;
+}
+</style>
